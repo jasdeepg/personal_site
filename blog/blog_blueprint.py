2d543a431bca097e09258644cc06d6b8c9dff4e7
Jasdeep Garcha <jasdeepsgarcha@gmail.com>
2020-03-23 09:04:08 -0600

    blog styling added

--- blog/blog_blueprint.py
+++ blog/blog_blueprint.py
@@ -4,10 +4,13 @@ from jinja2 import TemplateNotFound
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import config
+import datetime
 
 blog_app = Blueprint('blog', __name__, template_folder='templates/blog')
 
 client = ButterCMS(config.BUTTERCMS_KEY)
+
+#blog_app.jinja_env.globals.update(convert_unicode_datetime=convert_unicode_datetime)
 
 @blog_app.route('/')
 
@@ -17,18 +20,19 @@ def home(page=1):
     posts = response['data'] 
     next_page = response['meta']['next_page']
     previous_page = response['meta']['previous_page']
-    return render_template('blog.html', posts=posts, next_page=next_page, previous_page=previous_page)
+    return render_template('blog.html', posts=posts, next_page=next_page, previous_page=previous_page, convert_unicode_datetime=convert_unicode_datetime)
 
 
 @blog_app.route('/<slug>')
 def show_post(slug):
     response = client.posts.get(slug)
+    published_date = convert_unicode_datetime(response['data']['published'])
     try:
         post = response['data']
     except:
         # Post was not found
         abort(404)
-    return render_template('post.html', post=post)
+    return render_template('post.html', post=post, published_date=published_date)
 
 @blog_app.route('/category/<category_slug>')
 def show_category(category_slug):
@@ -39,3 +43,13 @@ def show_category(category_slug):
         # category was not found
         abort(404)
     return render_template('category.html', category=category)
+
+# convert unicode to datetime object
+def convert_unicode_datetime(date):
+    formatted_date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ");
+    return formatted_date;
+
+
+
+
+
