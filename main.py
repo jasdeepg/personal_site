be941f8637e447b73a0b6fbfd4f7393b79df0383
Jasdeep Garcha <jasdeepsgarcha@gmail.com>
2020-04-24 13:18:57 -0600

    added projects blueprint

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, render_template
 from blog.blog_blueprint import blog_app
+from projects.projects_blueprint import projects_app
 import config
 
 app = Flask(__name__)
 
 app.register_blueprint(blog_app, url_prefix='/blog')
+app.register_blueprint(projects_app, url_prefix='/projects')
 
 @app.route('/')
 def main():
@@ -19,10 +21,6 @@ def about():
 def about_ex():
     return render_template('about_ex.html')
 
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
-
 @app.route('/fb_test')
 def fb_test():
     return render_template('fb_test.html')
@@ -30,108 +28,6 @@ def fb_test():
 @app.route('/channel')
 def channel():
     return render_template('channel.html')
-
-#projects
-
-#2014
-@app.route('/lolliandpops')
-def lolliandpops():
-    return render_template('lolliandpops.html')
-
-#2013
-@app.route('/target')
-def target():
-    return render_template('target_accrual.html')
-
-@app.route('/pov')
-def pov():
-    return render_template('pov.html')
-
-@app.route('/chocolatetasting')
-def chocolatetasting():
-    return render_template('chocolatetasting.html')
-
-@app.route('/chocolateis')
-def chocolateis():
-    return render_template('chocolateis.html')
-
-
-#2012
-@app.route('/bounce')
-def bounce():
-    return render_template('bounce.html')
-
-@app.route('/image')
-def image():
-    return render_template('image_pixels.html')
-
-@app.route('/holdhands')
-def hold_hands():
-    return render_template('hold_hands.html')
-
-@app.route('/98lumens')
-def lumens():
-    return render_template('98lumens.html')
-
-@app.route('/doubleyou')
-def doubleyou():
-    return render_template('doubleyou.html')
-
-#2011
-@app.route('/ms')
-def microsoft():
-    return render_template('microsoft.html')
-
-@app.route('/msni')
-def msni():
-    return render_template('msni.html')
-
-#2010
-@app.route('/lenses')
-def lenses():
-    return render_template('lenses.html')
-
-#2009
-@app.route('/voice')
-def voice():
-    return render_template('voice.html')
-
-@app.route('/duron')
-def duron():
-    return render_template('duron.html')
-
-@app.route('/metoo')
-def metoo():
-    return render_template('metoo.html')
-
-@app.route('/teched')
-def teched():
-    return render_template('teched.html')
-
-#2008
-@app.route('/gravis')
-def gravis():
-    return render_template('gravis.html')
-
-@app.route('/water')
-def water():
-    return render_template('water.html')
-
-@app.route('/traffic')
-def traffic():
-    return render_template('traffic.html')
-
-#2007
-@app.route('/boe')
-def boe():
-    return render_template('boe.html')
-
-#2006
-
-#2005
-@app.route('/superconductor')
-def superconductor():
-    return render_template('superconductor.html')
 
 #CSV files
 @app.route('/distance.csv')
---
+++ projects/projects_blueprint.py
@@ -0,0 +1,39 @@
+from butter_cms import ButterCMS
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import config
+import datetime
+
+projects_app = Blueprint('projects', __name__, template_folder='templates/projects', static_folder='../static')
+
+client = ButterCMS(config.BUTTERCMS_KEY)
+
+#blog_app.jinja_env.globals.update(convert_unicode_datetime=convert_unicode_datetime)
+
+@projects_app.route('/')
+
+@projects_app.route('/page/<int:page>')
+def home(page=1):
+    response = client.content_fields.get(['projects'])
+    projects = response['data']['projects']
+    return render_template('projects_new.html', projects=projects, convert_unicode_datetime=convert_unicode_datetime)
+
+
+@projects_app.route('/<slug>')
+def show_post(slug):
+    response = client.content_fields.get(['projects'], {'fields.slug':slug})
+    date_start = convert_unicode_datetime(response['data']['projects'][0]['date_start'])
+    date_end = convert_unicode_datetime(response['data']['projects'][0]['date_end'])
+    try:
+        project = response['data']['projects'][0]
+    except:
+        # Post was not found
+        abort(404)
+    return render_template('project.html', project=project, date_start=date_start, date_end=date_end)
+
+# convert unicode to datetime object
+def convert_unicode_datetime(date):
+    formatted_date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S");
+    return formatted_date;
